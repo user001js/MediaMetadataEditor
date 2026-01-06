ba30c69854dd341dccf00a9dc8c074e8018da837
Apollo <101312928+user001js@users.noreply.github.com>
2026-01-06 15:11:24 -0300

    Add files via upload

---
+++ Views/PromptWindow.xaml.cs
@@ -0,0 +1,30 @@
+using System.Windows;
+
+namespace MediaMetadataEditor.Views
+{
+    public partial class PromptWindow : Window
+    {
+        public string? Result { get; private set; }
+        public PromptWindow(string message, string title = "Input", string defaultValue = "")
+        {
+            InitializeComponent();
+            Title = title;
+            LblMessage.Text = message;
+            TxtInput.Text = defaultValue;
+            Loaded += (s, e) => { TxtInput.Focus(); TxtInput.SelectAll(); };
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Result = TxtInput.Text ?? "";
+            DialogResult = true;
+            Close();
+        }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Result = null;
+            DialogResult = false;
+            Close();
+        }
+    }
+}
