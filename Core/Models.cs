aebcab483f296b93cba1276c6968eccaacb4eef8
Apollo <101312928+user001js@users.noreply.github.com>
2026-01-06 18:24:41 -0300

    Update Models.cs

--- Core/Models.cs
+++ Core/Models.cs
@@ -1,28 +1,14 @@
+using System;
 using System.Collections.Generic;
 
 namespace MediaMetadataEditor.Core
 {
-    public enum FieldStatus { Preview, Success, Partial, Failed, Unsupported }
-    public enum CapabilitySupport { Unknown, Supported, Conditional, Unsupported }
+    public enum CapabilitySupport { Unsupported, Partial, Supported }
 
-    public record Preset
+    public class Preset
     {
-        public string Name { get; init; } = "";
-        public Dictionary<string, string> Values { get; init; } = new();
-        public HashSet<string> CheckedFields { get; init; } = new();
-    }
-
-    public record FieldResult
-    {
-        public string Field { get; init; } = "";
-        public FieldStatus Status { get; init; } = FieldStatus.Preview;
-        public string Message { get; init; } = "";
-        public string? BackupPath { get; init; }
-    }
-
-    public record FileReport
-    {
-        public string FilePath { get; init; } = "";
-        public Dictionary<string, FieldResult> Fields { get; init; } = new();
+        public string Name { get; set; } = "";
+        public Dictionary<string, string> Values { get; set; } = new();
+        public HashSet<string> CheckedFields { get; set; } = new(StringComparer.OrdinalIgnoreCase);
     }
 }
